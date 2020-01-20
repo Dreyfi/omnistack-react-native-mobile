f405d06977582ac4de94685d477018fbca7347dc
Dreyfi F <dreyfi.sferreira@gmail.com>
2020-01-20 01:26:51 -0300

    Setup profile page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
-function Main() {
+function Main({ navigation }) {
 
     const [currentRegion, setCurrentRegion ] = useState(null);
 
@@ -36,6 +36,15 @@ function Main() {
     return (<MapView initialRegion={currentRegion} style={{ flex:1 }}>
         <Marker coordinate={{latitude: -23.5936717, longitude: -46.7917419}} >
             <Image style={styles.avatar} source={{uri: 'https://avatars2.githubusercontent.com/u/3505903?s=460&v=4'}} />
+            <Callout onPress={() => {
+                navigation.navigate('Profile', { github_username: 'dreyfi' })
+            }}>
+                <View style={styles.callout}>
+                    <Text style={styles.devName}>Dry F.</Text>
+                    <Text style={styles.devBio}>Fullstack developer PHP, Javascript and Flutter.</Text>
+                    <Text style={styles.devTechs}>PHP, Javascript and Flutter.</Text>
+                </View>
+            </Callout>
         </Marker>
     </MapView>)
 }
@@ -50,7 +59,26 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         borderWidth: 4,
         borderColor: '#FFF'
+    },
+
+    callout: {
+        width: 260,
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+
+    devBio: {
+        color: '#666',
+        marginTop: 5
+    },
+
+    devTechs: {
+        marginTop: 5
     }
+
 
 })
 
